d14045f58dde421d2e6f3874c0bc845d6edc3c39
alexpaps <121684889+alexpaps@users.noreply.github.com>
2023-07-09 02:26:03 +0300

    Update signIn.js

--- signIn.js
+++ signIn.js
@@ -1,48 +1,53 @@
 document.body.style.backgroundColor = "white";
 
 document.getElementById("DoneButton").addEventListener("click", function() {
-  let name = document.getElementById("Name").value;
-  let password = document.getElementById("Password").value;
-  let myList = JSON.parse(localStorage.getItem('data')) || [];
-  
-  if (name && password) {
+  let email1 = document.getElementById("Email").value;
+  let password1 = document.getElementById("Password").value;
+  // let myList = JSON.parse(localStorage.getItem('data')) || [{email:"alexpapadopoulos1011@gmail.com",password:"fineas9988"},{email:"hsbuvsahvs@gmail.com",password:"chzvhcv6348888"}];
+  let names = localStorage.getItem("myNames") || "";
+  let emails = localStorage.getItem("myEmails") || "";
+  let passwords = localStorage.getItem("myPasswords") || "";
+  const namesList = names.split(',') || [];
+  const emailsList = emails.split(',') || [];
+  const passwordsList = passwords.split(',') || [];
+  const listLength = emailsList.length;
+  let errorNum = 0;
+  console.log(names,emails,passwords,namesList,emailsList,passwordsList);
+
+  if (email1 && password1 && listLength > 0) {
     let userData = {
-          name: name,
-          password: password
+          email:String(email1),
+          password:String(password1)
+    };
+
+    let i =0;
+    while(i < listLength){
+        if (emailsList[i] == userData.email && passwordsList[i] == userData.password){
+          // Display success message
+          alert("Logged in!Hii mr/miss "+namesList[i]);
+          window.location.href = "mainPage.html";
+          errorNum = 1;
+          break;
+        }else if(passwordsList[i] != userData.password && emailsList[i] == userData.email){
+          errorNum = 2;
+          break;
+        }
+        errorNum = 3;
+        i++;
       };
 
-      // // Using push() methods
-      // myList.push(userData);
-
-      // // Output the updated list
-      // console.log(myList);
-
-      // localStorage.setItem('data', JSON.stringify(myList));
+      if(errorNum == 2){
+        alert("Incorrect password!");
+      }else if(errorNum == 3){
+        alert("Not valid account!");
+      }
+      
 
       // Clear the input fields
-      document.getElementById("Name").value = "";
+      document.getElementById("Email").value = "";
       document.getElementById("Password").value = "";
-      
-      
-      // Display success message
-      alert("Data saved successfully!");
+
   } else {
       alert("Please enter both name and password.");
   }
-});
-
-document.getElementById("Name").addEventListener("click",function(){
-    // Get a reference to the div element
-  const divElement = document.getElementById("nameView");
-
-  // Add a class to the element
-  divElement.classList.add("myClass");
-});
-
-document.getElementById("Password").addEventListener("click",function(){
-  // Get a reference to the div element
-const divElement = document.getElementById("nameView");
-
-// Add a class to the element
-divElement.classList.remove("myClass");
 });
